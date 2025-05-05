9fe1588f18b4a9e3877ca89f66bc148a22e89e6b
farsbrayek3 <faresbrayek3@gmail.com>
2025-05-05 14:42:07 +0100

    fix: add Swagger documentation for user signup route

--- src/modules/auth/routes/signupRoutes.js
+++ src/modules/auth/routes/signupRoutes.js
@@ -1,3 +1,18 @@
+/**
+ * @swagger
+ * /api/auth/signup:
+ *   post:
+ *     summary: Register a new user
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/UserSignup'
+ *     responses:
+ *       201:
+ *         description: User created
+ */
 const express = require("express");
 const SignupController = require("../controllers/signupController");
 const router = express.Router();
